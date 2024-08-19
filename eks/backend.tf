d651d08ba5d358c42321b3fd5ce05164693e3537
Kareti Venkata Vinay Kumar <21pa1a0574@vishnu.edu.in>
2024-08-19 12:04:44 +0530

    adding jenkins files

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "vinay-terraform-74"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    # dynamodb_table = "Lock-Files"
     encrypt        = true
   }
 }
